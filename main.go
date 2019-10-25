2baba908c938d30dc1cadb6abeea66c3c88a6807
ego008 <ego008@gmail.com>
2019-10-25 20:29:58 +0800

    添加timeout

--- main.go
+++ main.go
@@ -120,6 +120,8 @@ func main() {
 			Handler:        httpgzip.NewHandler(root, nil),
 			TLSConfig:      tlsCf,
 			MaxHeaderBytes: int(app.Cf.Site.UploadMaxSizeByte),
+			ReadTimeout: 5 * time.Second,
+			WriteTimeout: 10 * time.Second,
 		}
 
 		go func() {
@@ -132,7 +134,12 @@ func main() {
 
 	} else {
 		// http
-		srv = &http.Server{Addr: ":" + strconv.Itoa(mcf.HttpPort), Handler: root}
+		srv = &http.Server{
+			Addr: ":" + strconv.Itoa(mcf.HttpPort), 
+			Handler: root,
+			ReadTimeout: 5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+		}
 		go func() {
 			log.Fatal(srv.ListenAndServe())
 		}()
